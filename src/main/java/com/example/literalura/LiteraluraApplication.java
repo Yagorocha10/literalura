a33ec588b9bf20cadca4c5edb763a55dfb49c810
Yagorocha10 <yagodasilvarocha30@gmail.com>
2026-01-16 16:44:19 -0300

    solitacao a API concluida

--- src/main/java/com/example/literalura/LiteraluraApplication.java
+++ src/main/java/com/example/literalura/LiteraluraApplication.java
@@ -1,13 +1,24 @@
 package com.example.literalura;
 
+import com.example.literalura.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+
+		ConsumoApi consumoApi = new ConsumoApi();
+		var consumindoApi = consumoApi.obterDados("https://gutendex.com/books/1/");
+		System.out.println(consumindoApi);
+
+	}
 }
---
+++ src/main/java/com/example/literalura/principal/Principal.java
@@ -0,0 +1,4 @@
+package com.example.literalura.principal;
+
+public class Principal {
+}
